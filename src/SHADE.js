7e751eced7a6bfa02afa79e36398e8cf431bef79
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-10-25 01:51:09 +0200

    0.3.3

--- src/SHADE.js
+++ src/SHADE.js
@@ -9,12 +9,16 @@ const config_defaults = {
 };
 
 export default class SHADE {
-    version = '0.3.2';
+
+    version = '0.3.3';
 
     mouseX = 0;
     mouseY = 0;
 
     time = performance.now();
+
+    fragment_glsl = ``;
+    vertex_glsl = `attribute vec4 _vertices; void main() { gl_Position = _vertices; }`;
     
     constructor(canvas_element, config_override){
         this.config = {...config_defaults, ...config_override};
@@ -30,6 +34,8 @@ export default class SHADE {
         this.canvas3D = new OffscreenCanvas(this.canvas.width, this.canvas.height);
         this.context3D = this.canvas3D.getContext('webgl2', {antialias: true, alpha: true});
 
+        // 
+
         this.canvas.onmousemove = event => {
             this.mouseX = event.clientX - this.bcr.left;
             this.mouseY = event.clientY - this.bcr.top;
@@ -43,9 +49,38 @@ export default class SHADE {
         window.onresize = event => {
             this.#resizeCanvas();
         }
+
+        // 
+
+        this.empty = function() {}
+    
+        this.once2D = function() {}        
+        this.loop2D = function() {}
+
+        this.once3D = function() {
+            this.program = this.createProgram(this.createShader(this.context3D.VERTEX_SHADER, this.vertex_glsl), this.createShader(this.context3D.FRAGMENT_SHADER, this.fragment_glsl));
+            this.context3D.useProgram(this.program);
+
+            this.vertexData = [ -1, -1, +1, -1, +1, +1, -1, +1, -1, -1, +1, +1 ];
+
+            this.vertexArray = this.context3D.getAttribLocation(this.program, '_vertices');
+            this.context3D.bindBuffer(this.context3D.ARRAY_BUFFER, this.context3D.createBuffer());
+            this.context3D.bufferData(this.context3D.ARRAY_BUFFER, new Float32Array(this.vertexData), this.context3D.STATIC_DRAW);
+            this.context3D.enableVertexAttribArray(this.vertexArray);
+            this.context3D.vertexAttribPointer(this.vertexArray, 2, this.context3D.FLOAT, false, 0, 0);  
+            this.ST_canvasResolution = this.context3D.getUniformLocation(this.program, 'iResolution');
+            this.ST_mousePosition = this.context3D.getUniformLocation(this.program, 'iMouse');
+            this.ST_currentTime = this.context3D.getUniformLocation(this.program, 'iTime');
+        }
+        this.loop3D = function() {
+            this.context3D.uniform2f(this.ST_canvasResolution, this.canvas.width, this.canvas.height);
+            this.context3D.uniform2f(this.ST_mousePosition, this.mouseX, this.mouseY);
+            this.context3D.uniform1f(this.ST_currentTime, this.time * 0.001);
+            this.context3D.drawArrays(this.context3D.TRIANGLES, 0, this.vertexData.length/2);
+        }
     }
 
-    // private methods
+    //
 
     #parseSize(input) {
         return parseInt(input) ? parseInt(input) : {
@@ -61,28 +96,33 @@ export default class SHADE {
         this.bcr = this.canvas.getBoundingClientRect()
 
         this.context3D.viewport(0, 0, this.canvas.width, this.canvas.height);
+
+        if(this.loop2D.toString() == this.empty.toString()) {
+            this.once2D();
+        }
+
+        if(this.loop3D.toString() == this.empty.toString()) {
+            this.once3D();
+        }
+
     }
 
-    #loopRenderer() {        
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.context2D.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.context3D.clear(this.context3D.COLOR_BUFFER_BIT);
-       
-        this.time = performance.now();        
+    #loopRenderer() {
+        if(this.loop2D.toString() != this.empty.toString()){
+            this.context2D.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.loop2D();
+        }
 
-        this.loop2D();
-
-        this.context3D.uniform2f(this.ST_canvasResolution, this.canvas.width, this.canvas.height);
-        this.context3D.uniform2f(this.ST_mousePosition, this.mouseX, this.mouseY);
-        this.context3D.uniform1f(this.ST_currentTime, this.time * 0.001);
         this.loop3D();
+
+        this.time = performance.now();        
 
         this.context.drawImage(this.canvas3D,0,0);
         this.context.drawImage(this.canvas2D,0,0);
         requestAnimationFrame(this.#loopRenderer.bind(this));
     }
 
-    // public methods
+    //
 
     createShader(type, source) {
         let shader = this.context3D.createShader(type);
@@ -113,21 +153,13 @@ export default class SHADE {
         this.context3D.deleteProgram(program);
     }
 
-    setup2D() {}
-    loop2D() {}
-
-    setup3D() {}
-    loop3D() {}
+    //
     
     run() {
-        this.setup2D();
-
-        this.setup3D();
-        this.ST_canvasResolution = this.context3D.getUniformLocation(this.program, 'iResolution');
-        this.ST_mousePosition = this.context3D.getUniformLocation(this.program, 'iMouse');
-        this.ST_currentTime = this.context3D.getUniformLocation(this.program, 'iTime');
-
         this.#resizeCanvas();
+
+        this.once2D();
+        this.once3D();
         setTimeout(() => { this.#loopRenderer(); }, 100);
     }
 }
