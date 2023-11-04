3fbeea0b33d43f15339a8d0d31c95aff0bc30014
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-11-04 07:44:07 +0100

    0.3.7

--- examples/js/example1.js
+++ examples/js/example1.js
@@ -7,8 +7,10 @@ let example = new SHADE('EXAMPLE1', {width: 'maxWidth', height: 'maxHeight', is_
 example.fragment_shader = await example.loadShader('glsl/example1.glsl');
 
 example.once2D = function() {
-    this.context2D.fillStyle = 'rgba(0,0,0,.25)';
+    this.context2D.fillStyle = 'rgba(255,0,0,.25)';
     this.context2D.fillRect(0, 0, this.canvas.width/2, this.canvas.height/2);
+    this.context2D.fillStyle = 'rgba(0,255,0,.25)';
+    this.context2D.fillRect(this.canvas.width/2, this.canvas.height/2, this.canvas.width, this.canvas.height);
 }
 
 //
