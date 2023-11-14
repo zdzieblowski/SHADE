851e7931bf1cd56b8b913a50e0a32e7e12f4f104
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-11-14 03:11:53 +0100

    0.3.11

--- src/SHADE.js
+++ src/SHADE.js
@@ -18,7 +18,7 @@ const config_defaults = {
 };
 
 export default class SHADE {
-    version = '0.3.10';
+    version = '0.3.11';
 
     mouseX = 0;
     mouseY = 0;
@@ -26,7 +26,7 @@ export default class SHADE {
     mouseW = 0;
     mouseDown = false;
 
-    time = Date.now();
+    time = performance.now();
     frame = 0;
     prev_date = new Date();
 
@@ -37,30 +37,30 @@ export default class SHADE {
 
     fragment_shader = ``;
     vertex_shader = ``;
-    
-    constructor(canvas_element, config_override){
-        this.config = {...config_defaults, ...config_override};
+
+    constructor(canvas_element, config_override) {
+        this.config = { ...config_defaults, ...config_override };
 
         this.canvas = document.getElementById(canvas_element);
-        this.context = this.canvas.getContext('2d', {antialias: this.config.antialias, alpha: this.config.alpha})
-        
+        this.context = this.canvas.getContext('2d', { antialias: this.config.antialias, alpha: this.config.alpha })
+
         // 
 
         this.canvas2D = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-        this.context2D = this.canvas2D.getContext('2d', {antialias: this.config.antialias2D, alpha: this.config.alpha2D});
+        this.context2D = this.canvas2D.getContext('2d', { antialias: this.config.antialias2D, alpha: this.config.alpha2D });
 
         this.canvas3D = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-        this.context3D = this.canvas3D.getContext(this.config.api3D, {antialias: this.config.antialias3D, alpha: this.config.alpha3D});
+        this.context3D = this.canvas3D.getContext(this.config.api3D, { antialias: this.config.antialias3D, alpha: this.config.alpha3D });
 
         // 
 
         this.canvas.onmousemove = event => {
             this.bcr = this.canvas.getBoundingClientRect();
-            if(!this.config.is_shadertoy) {
+            if (!this.config.is_shadertoy) {
                 this.mouseX = event.clientX - this.bcr.left;
                 this.mouseY = event.clientY - this.bcr.top;
             } else {
-                if(this.mouseDown){
+                if (this.mouseDown) {
                     this.mouseX = event.clientX - this.bcr.left;
                     this.mouseY = this.bcr.bottom - event.clientY;
                 }
@@ -70,14 +70,22 @@ export default class SHADE {
         this.canvas.onmousedown = event => {
             this.bcr = this.canvas.getBoundingClientRect();
             this.mouseDown = true;
+
+            if (this.config.is_shadertoy) {
+                this.mouseSignalDown = true;
+            }
+
             this.mouseX = this.mouseZ = event.clientX - this.bcr.left;
             this.mouseY = this.mouseW = this.bcr.bottom - event.clientY;
         }
 
         this.canvas.onmouseup = event => {
             this.mouseDown = false;
-            this.mouseZ = 0;
-            this.mouseW = 0;
+            if (!this.config.is_shadertoy) {
+                this.mouseZ = 0;
+                this.mouseW = 0;
+            }
+
         }
 
         const resizeEvent = new ResizeObserver((entries) => {
@@ -95,7 +103,7 @@ export default class SHADE {
         this.ST_once3D = function() {
             this.program = this.createProgram(this.createShader(this.context3D.VERTEX_SHADER, this.vertex_shader), this.createShader(this.context3D.FRAGMENT_SHADER, this.fragment_shader));
             this.context3D.useProgram(this.program);
-            this.vertexData = [ -1,-1,+1,-1,+1,+1,-1,+1,-1,-1,+1,+1 ];
+            this.vertexData = [-1, -1, 1, -1, 1, 1, -1, 1, -1, -1, 1, 1];
             this.vertexArray = this.context3D.getAttribLocation(this.program, '_vertices');
             this.context3D.bindBuffer(this.context3D.ARRAY_BUFFER, this.context3D.createBuffer());
             this.context3D.bufferData(this.context3D.ARRAY_BUFFER, new Float32Array(this.vertexData), this.context3D.STATIC_DRAW);
@@ -115,20 +123,19 @@ export default class SHADE {
             let delta = date - this.prev_date;
             this.context3D.uniform4f(this.ST_currentDate, date.getFullYear(), date.getMonth(), date.getDate(), (date.getHours() * 3600. + date.getMinutes() * 60. + date.getSeconds() + date.getMilliseconds() / 1000.))
             this.context3D.uniform1i(this.ST_currentFrame, this.frame);
-            this.context3D.uniform1f(this.ST_currentFrameRate, 1000./delta);
+            this.context3D.uniform1f(this.ST_currentFrameRate, 1000. / delta);
             this.context3D.uniform4f(this.ST_mousePosition, this.mouseX, this.mouseY, this.mouseZ, this.mouseW);
             this.context3D.uniform3f(this.ST_canvasResolution, this.canvas.width, this.canvas.height, 1.0);
             this.context3D.uniform1f(this.ST_currentTime, this.time * 0.001);
             this.context3D.uniform1f(this.ST_currentTimeDelta, delta * 0.001);
-            
-            this.context3D.drawArrays(this.context3D.TRIANGLES, 0, this.vertexData.length/2);
+            this.context3D.drawArrays(this.context3D.TRIANGLES, 0, this.vertexData.length / 2);
             this.frame++;
             this.prev_date = date;
         }
 
         //
 
-        this.empty = function() {}
+        this.empty  = function() {}
         this.once2D = function() {}
         this.loop2D = function() {}
         this.once3D = function() {}
@@ -149,39 +156,54 @@ export default class SHADE {
         this.canvas2D.height = this.canvas3D.height = this.canvas.height = this.#parseSize(this.config.height);
 
         this.bcr = this.canvas.getBoundingClientRect();
-        
+
         this.context3D.viewport(0, 0, this.canvas.width, this.canvas.height);
 
-        if(this.config.is_shadertoy) {
-            this.mouseX = this.canvas.width/2;
-            this.mouseY = this.canvas.height/2;
+        if (this.config.is_shadertoy) {
+            this.mouseX = this.canvas.width / 2;
+            this.mouseY = this.canvas.height / 2;
         }
 
-        if(this.l2E) {
+        if (this.l2E) {
             this.once2D();
         }
 
-        if(this.l3E) {
+        if (this.l3E) {
             this.once3D();
         }
     }
 
     #loopRenderer() {
-        if(!this.l2E){
+        if (this.config.is_shadertoy) {
+            this.mouseZ = Math.abs(this.mouseZ);
+            this.mouseW = Math.abs(this.mouseW);
+
+            if (!this.mouseDown) {
+                this.mouseZ = -this.mouseZ;
+            }
+
+            if (!this.mouseSignalDown) {
+                this.mouseW = -this.mouseW;
+            }
+
+            this.mouseSignalDown = false;
+        }
+
+        if (!this.l2E) {
             this.context2D.clearRect(0, 0, this.canvas.width, this.canvas.height);
             this.loop2D();
         }
 
-        if(!this.l3E){
+        if (!this.l3E) {
             this.loop3D();
         }
 
-        this.time = performance.now();        
+        this.time = performance.now();
 
         this.context.drawImage(this.canvas3D, 0, 0);
         this.context.drawImage(this.canvas2D, 0, 0);
 
-        if(!this.l2E || !this.l3E) {
+        if (!this.l2E || !this.l3E) {
             requestAnimationFrame(this.#loopRenderer.bind(this));
         }
     }
@@ -201,35 +223,35 @@ export default class SHADE {
 
         if (this.context3D.getShaderParameter(shader, this.context3D.COMPILE_STATUS)) {
             return shader;
-        }            
-        
+        }
+
         console.log(this.context3D.getShaderInfoLog(shader));
         this.context3D.deleteShader(shader);
     }
 
     createProgram(vertex_shader, fragment_shader) {
         let program = this.context3D.createProgram();
-        
+
         this.context3D.attachShader(program, vertex_shader);
         this.context3D.attachShader(program, fragment_shader);
         this.context3D.linkProgram(program);
-        
+
         if (this.context3D.getProgramParameter(program, this.context3D.LINK_STATUS)) {
             return program;
-        }            
-        
+        }
+
         console.log(this.context3D.getProgramInfoLog(program));
         this.context3D.deleteProgram(program);
     }
 
     //
-    
+
     run() {
         this.#resizeCanvas();
 
         //
 
-        if(this.config.is_shadertoy) {
+        if (this.config.is_shadertoy) {
             this.vertex_shader = this.ST_vertex_shader;
             this.once3D = this.ST_once3D;
             this.loop3D = this.ST_loop3D;
